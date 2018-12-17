f9420f7fff1dfb099e8a8d35e96f90170b7b343a
vipul <vipulvalvi51@gmail.com>
2018-12-17 04:07:33 -0800

    Initial

---
+++ tryit/src/main/java/com/tryit/TryitApplication.java
@@ -0,0 +1,14 @@
+package com.tryit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TryitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TryitApplication.class, args);
+	}
+
+}
+
